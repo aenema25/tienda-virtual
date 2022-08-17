c12b5f094751a1c9555ce87663ccc996d2fe912c
Oscar Uribe <aenemapusher24@gmail.com>
2022-08-17 17:01:35 -0400

    Added navbar component

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,29 @@
+import './navbar.css'
+
+const Navbar = () => (
+    <nav className='navbar'>
+        <div>
+            Tienda Virtual
+        </div>
+        <ul className='navigation'>
+            <li>
+                <a href='#'>
+                    Inicio
+                </a>
+            </li>
+            <li>
+                <a href='#'>
+                    Contacto
+                </a>
+            </li>
+            <li>
+                <a href='#'>
+                    Carro
+                </a>
+            </li>
+        </ul>
+    </nav>
+)
+
+
+export default Navbar
