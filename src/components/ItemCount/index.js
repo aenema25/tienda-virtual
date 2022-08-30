2ce3646cb7151ce0e720b8cf1b994e94bdabd2c0
Oscar Uribe <aenemapusher24@gmail.com>
2022-08-29 20:22:37 -0400

    Added ItemCount component

---
+++ src/components/ItemCount/index.js
@@ -0,0 +1,49 @@
+import { useState } from "react"
+import './itemcount.css'
+
+const ItemCount = ({ producto, initialStock, initial }) => {
+    const [cantidad, setCantidad] = useState(initial)
+    const [empty, setEmpty] = useState(false)
+    const [stock, setStock] = useState(initialStock)
+
+    const addUp = () => {
+        if (cantidad < stock) {
+            setCantidad(cantidad + 1)
+            setEmpty(false)
+        } else {
+            setEmpty(true)
+        }
+    }
+    const restDown = () => {
+        if (cantidad > 0) {
+            setCantidad(cantidad - 1)
+        }
+        setEmpty(false)
+    }
+
+    const updateStock = () => {
+        setStock(stock - cantidad)
+        setCantidad(initial)
+    }
+
+    return (
+        <div className="productoCard">
+            <div className="productoCardTitle">
+                {producto.name}
+            </div>
+            <div className="productoStock">
+                <button className="stockButtonLeft" onClick={restDown}>-</button>
+                <div className="cantidad">
+                    {cantidad}
+                </div>
+                <button className="stockButtonRight" onClick={addUp}>+</button>
+            </div>
+            <div className="emptyStockMessage">
+                {empty && 'Se alcanzo la maxima cantidad de items para este producto'}
+            </div>
+            <button className="cartAddButton" onClick={updateStock}>Agregar al carrito</button>
+        </div>
+    )
+}
+
+export default ItemCount 
--- src/components/ItemListContainer/index.js
+++ src/components/ItemListContainer/index.js
@@ -1,9 +1,36 @@
+import ItemCount from '../ItemCount'
 import './itemlistcontainer.css'
 
-const ItemListContainer = ({greetings}) =>{
+const productos = [
+    {
+        name: 'Zapatilla roja',
+        stock: 20
+    },
+    {
+        name: 'Zapatilla negra',
+        stock: 15
+    },
+    {
+        name: 'Cortaviento morado',
+        stock: 9
+    },
+    {
+        name: 'Parka verde',
+        stock: 5
+    }
+]
+
+const ItemListContainer = ({ greetings }) => {
     return (
         <div className='container'>
             {greetings}
+            <div className='productosContainer'>
+                {
+                    productos.map(producto => (
+                        <ItemCount producto={producto} initialStock={producto.stock} initial={0} />
+                    ))
+                }
+            </div>
         </div>
     )
 }
